0f94f56c428f3d4b264c2ccdaa6abbbae5b11e60
Mutombe <simbarashemutombe1@gmail.com>
2025-02-11 22:30:08 +0200

    admin dashboard

--- hospital_management/core/admin.py
+++ hospital_management/core/admin.py
@@ -1,3 +1,31 @@
+# core/admin.py
 from django.contrib import admin
+from .models import User, Patient
 
-# Register your models here.
+class AdminUserOverview(admin.ModelAdmin):
+    list_display = (
+        "role",
+        "username",
+        "email",
+    )
+    search_fields = (
+        "username",
+    )
+
+class AdminPatientOverview(admin.ModelAdmin):
+    list_display = (
+        "mrn",
+        "date_of_birth",
+        "blood_type",
+    )
+    search_fields = (
+        "blood_type",
+    )
+    ordering = ("blood_type",)
+    list_filter = (
+        "blood_type",
+    )
+
+
+admin.site.register(User, AdminUserOverview)
+admin.site.register(Patient, AdminPatientOverview)
